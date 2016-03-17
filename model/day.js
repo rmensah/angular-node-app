5146bdcdcdd0e938e485d25228a0063dae4ab8c2
RM <rm@RMs-MacBook-Pro.fios-router.home>
2016-03-16 21:39:58 -0400

    moved dir and changed dayModel.js to day.js

---
+++ model/day.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+/*
+Day is an object in a collection
+
+    Day belongs to -> Schedule
+
+*/
+
+var daySchema = new Schema({
+    dayDate: String,
+    hours: Schema.Types.Mixed
+});
+
+
+var Day = mongoose.model('Day', daySchema);
+module.exports = Day;
+
